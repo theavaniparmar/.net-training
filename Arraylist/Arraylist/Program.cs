7fb1284dce4386742134c3b29110f0e8d503daec
Avani Parmar <133568468+theavaniparmar@users.noreply.github.com>
2025-01-31 17:57:03 +0530

    perform arraylist

---
+++ Arraylist/Arraylist/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Arraylist
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+
+            list.Add(1);
+            list.Add(true);
+            list.Add("abc");
+            list.Add('c');
+            list.Add(3.14);
+
+            foreach (var i in list)
+            {
+                Console.WriteLine("Array element:"+i);
+            }
+
+            list.AddRange(new object[] { 100, "avani", 6.7 });
+            Console.WriteLine(list[6]);
+            Console.WriteLine(list.Contains(100));
+            Console.WriteLine(list.IndexOf(6.7));
+
+            list[1] = "modify";
+            list.Insert(0, 'd');
+            Console.WriteLine("\n new   arraylist:");
+            foreach (var i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            list.Remove(3.14);
+            Console.WriteLine("\n new   arraylist:");
+            foreach (var i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            ArrayList list1 = new ArrayList() { 2, 8, 5, 9 };
+            list1.Sort();
+           
+            Console.WriteLine("new sorted  arraylist:");
+            foreach (var i1 in list1)
+            {
+                Console.WriteLine(i1);
+            }
+
+            list1.Reverse();
+            Console.WriteLine("new reverse  arraylist:");
+            foreach (var i1 in list1)
+            {
+                Console.WriteLine(i1);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Arraylist/Arraylist/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Arraylist")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Arraylist")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("341570b0-53ee-4f1f-88db-fb507a8a6aed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
