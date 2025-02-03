768282535c5a914aa2771fb81417d641f91f62f6
Avani Parmar <133568468+theavaniparmar@users.noreply.github.com>
2025-02-03 14:40:39 +0530

    perform hashset

---
+++ hashsetgeneric/hashsetgeneric/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashsetgeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> hs1 = new HashSet<string>();
+            hs1.Add("avani");
+            hs1.Add("saniya");
+            hs1.Add("nil");
+
+            Console.WriteLine("hashset1 elements:");
+            foreach (string s in hs1) Console.WriteLine(s);
+
+            HashSet<string> hs2 = new HashSet<string>();
+            hs2.Add("abc");
+            hs2.Add("avani");
+            hs2.Add("pqr");
+
+            Console.WriteLine("hashset2 elements:");
+            foreach (string i in hs2) { Console.WriteLine(i); }
+
+            hs1.IntersectWith(hs2);
+            Console.WriteLine("after intersection of hashset:");
+                foreach (string i in hs1) Console.WriteLine(i);
+
+            Console.ReadKey();
+
+        }
+    }
+}
