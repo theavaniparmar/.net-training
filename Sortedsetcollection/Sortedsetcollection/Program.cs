6033666298d0ce854b47451dad5daa9ee85ca29c
Avani Parmar <133568468+theavaniparmar@users.noreply.github.com>
2025-02-03 15:02:11 +0530

    sorted set operations

---
+++ Sortedsetcollection/Sortedsetcollection/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortedsetcollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<int> list = new SortedSet<int>
+            { 1,3,67,56,90,12,23,78,6,9,};
+
+            Console.WriteLine("sorted set elements:");
+            foreach(int i in list) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"minimum element:{list.Min}");
+            Console.WriteLine($"maximum element:{list.Max}");
+
+            Console.ReadKey();
+        }
+    }
+}
