2833e8add7f75658445ac809a7af66df3efba7fa
Avani Parmar <133568468+theavaniparmar@users.noreply.github.com>
2025-02-04 14:22:54 +0530

    LSP example

---
+++ LSPexample/LSPexample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSPexample
+{
+    public abstract class Shape
+    {
+        public abstract int Area();
+    }
+
+    public class Rectangle : Shape
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+
+        public override int Area()
+        {
+            return width * height;
+        }
+    }
+    public class Square : Shape
+    {
+        public int length;
+        public override int Area() { return length * length; }
+    }
+        public class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle
+            { width = 5, height = 2 };
+            Console.WriteLine($"Area of Rectangle:{rectangle.Area()}");
+
+            Square square = new Square
+            { length = 3 };
+            Console.WriteLine($"Area of Square: {square.Area()}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
